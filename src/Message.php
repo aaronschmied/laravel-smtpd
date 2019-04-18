21fe95d2a1bc828f7d929c34b27782c9d555cb0f
Aaron Schmied <aaron@pro-sales.ch>
2019-04-18 19:38:01 +0200

    Instantiate from a zend message.

--- src/Message.php
+++ src/Message.php
@@ -9,10 +9,46 @@
 namespace Smtpd;
 
 
-class Message
+
+
+use Illuminate\Mail\Mailable;
+
+class Message extends Mailable
 {
-    public static function make(\Zend\Mail\Message $message)
+    /**
+     * @var \Zend\Mail\Message
+     */
+    protected $zendMessage;
+
+    /**
+     * Set the zend message.
+     *
+     * @param \Zend\Mail\Message $zendMessage
+     *
+     * @return $this
+     */
+    public function setZendMessage(\Zend\Mail\Message $zendMessage)
     {
-        
+        $this->zendMessage = $zendMessage;
+
+        return $this;
+    }
+
+    /**
+     * Create a new message instance from the given zend message
+     *
+     * @param \Zend\Mail\Message $message
+     * @param string             $from
+     * @param array              $recipients
+     *
+     * @return Message
+     */
+    public static function makeFrom(\Zend\Mail\Message $message, string $from, array $recipients)
+    {
+        return (new static())
+            ->from($from)
+            ->to($recipients)
+            ->html($message->getBody())
+            ->setZendMessage($message);
     }
 }
