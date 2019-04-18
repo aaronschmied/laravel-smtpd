c8f3cabaccbea54bd6047246b4737c052148c372
Aaron Schmied <aaron@pro-sales.ch>
2019-04-18 19:14:21 +0200

    Added the command log proxy to print the logger messages to the console.

---
+++ src/Logging/CommandLogProxy.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ * Copyright: © 2019 Pro Sales AG
+ * Author: Aaron Schmied <aaron@pro-sales.ch>
+ * Date: 2019-04-18
+ * Time: 15:06
+ */
+
+namespace Smtpd\Logging;
+
+
+use Illuminate\Console\Command;
+use Psr\Log\LoggerInterface;
+use Psr\Log\LoggerTrait;
+use Psr\Log\LogLevel;
+
+class CommandLogProxy implements LoggerInterface
+{
+    use LoggerTrait;
+
+    /**
+     * @var Command
+     */
+    protected $command;
+
+    /**
+     * CommandOutLogger constructor.
+     *
+     * @param Command $command
+     */
+    public function __construct(Command $command)
+    {
+        $this->command = $command;
+    }
+
+    /**
+     * Log a message to the command
+     *
+     * @param mixed  $level
+     * @param string $message
+     * @param array  $context
+     */
+    public function log($level, $message, array $context = [])
+    {
+        if (! empty($context)) {
+            $message .= " " . json_encode($context);
+        }
+
+        switch ($level) {
+            case LogLevel::EMERGENCY:
+            case LogLevel::ALERT:
+            case LogLevel::CRITICAL:
+            case LogLevel::ERROR:
+                $this->command->error($message);
+                break;
+            case LogLevel::WARNING:
+                $this->command->warn($message);
+                break;
+            default:
+                $this->command->info($message);
+        }
+    }
+}
