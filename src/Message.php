ce0a788f3afe8490576801291027e7a78af64a94
Aaron Schmied <aaron@pro-sales.ch>
2019-04-18 19:14:45 +0200

    Created the message class.

---
+++ src/Message.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Copyright: © 2019 Pro Sales AG
+ * Author: Aaron Schmied <aaron@pro-sales.ch>
+ * Date: 2019-04-18
+ * Time: 12:00
+ */
+
+namespace Smtpd;
+
+
+class Message
+{
+    public static function make(\Zend\Mail\Message $message)
+    {
+        
+    }
+}
